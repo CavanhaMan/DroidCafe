1edc4f5b467543274e85d866215cdb507599dab4
CavanhaMan <cavanhaman@hotmail.com>
2018-10-25 10:57:52 -0300

    Adicionando arquivos - ERRO no layout!

--- app/src/main/java/com/example/android/droidcafeinput/MainActivity.java
+++ app/src/main/java/com/example/android/droidcafeinput/MainActivity.java
@@ -1,24 +1,53 @@
-package com.example.aluno.droidcafe;
-/* CavanhaMan */
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.droidcafeinput;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+/**
+ * This app demonstrates images used as buttons and a floating action button to
+ * use an intent to launch a second activity. The app lets a user tap an image
+ * to make a choice. The app displays a Toast message showing the user’s choice
+ * and sends the choice as data with an intent to launch the second activity.
+ */
 public class MainActivity extends AppCompatActivity {
-    
-	// Tag for the intent extra.
-    public static final String EXTRA_MESSAGE = "com.example.android.droidcafeinput.extra.MESSAGE";
+
+    // Tag for the intent extra.
+    public static final String EXTRA_MESSAGE =
+            "com.example.android.droidcafeinput.extra.MESSAGE";
 
     // The order message, displayed in the Toast and sent to the new Activity.
     private String mOrderMessage;
-	
+
+    /**
+     * Creates the content view, the toolbar, and the floating action button.
+     *
+     * This method is provided in the Basic Activity template.
+     *
+     * @param savedInstanceState Saved instance state bundle.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,8 +66,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-	
-	/**
+
+    /**
      * Inflates the menu, and adds items to the action bar if it is present.
      *
      * @param menu Menu to inflate.
